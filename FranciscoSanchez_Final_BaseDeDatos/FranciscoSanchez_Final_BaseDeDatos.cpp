4d086d6c36ab0751f27ad3fbc8b58aeb91ee39fb
pancho404 <franciscosanchez2101@gmail.com>
2023-02-24 16:16:11 -0300

    Created secondary functions.

--- FranciscoSanchez_Final_BaseDeDatos/FranciscoSanchez_Final_BaseDeDatos.cpp
+++ FranciscoSanchez_Final_BaseDeDatos/FranciscoSanchez_Final_BaseDeDatos.cpp
@@ -5,3 +5,31 @@
 enum class actions { ADD=1, UPDATE, CHECK, REMOVE};
 
 
+void enterData(MYSQL* connection);
+void eraseData(MYSQL* connection);
+void updateData(MYSQL* connection);
+void checkData(MYSQL* connection);
+
+
+
+
+
+void enterData(MYSQL* connection)
+{
+
+}
+
+void eraseData(MYSQL* connection)
+{
+
+}
+
+void updateData(MYSQL* connection)
+{
+
+}
+
+void vheckData(MYSQL* connection)
+{
+
+}
