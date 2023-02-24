6e7c830bdd726545896930430139a6aec7d608bf
pancho404 <franciscosanchez2101@gmail.com>
2023-02-24 16:41:03 -0300

    Fixed some mistakes and separated the checkData function between name and email.

--- FranciscoSanchez_Final_BaseDeDatos/FranciscoSanchez_Final_BaseDeDatos.cpp
+++ FranciscoSanchez_Final_BaseDeDatos/FranciscoSanchez_Final_BaseDeDatos.cpp
@@ -8,11 +8,15 @@ enum class actions { ADD=1, UPDATE, CHECK, REMOVE};
 void enterData(MYSQL* connection);
 void eraseData(MYSQL* connection);
 void updateData(MYSQL* connection);
-std::string checkData(MYSQL* connection, std::string& name);
+std::string checkName(MYSQL* connection, std::string& name);
+std::string checkEmail(MYSQL* connection, std::string& email);
 
 
 int main()
 {
+
+    std::string name, email;
+
     MYSQL* connection = mysql_init(NULL);
     if (!mysql_real_connect(connection, "localhost", "root", "password", "mydb", 3306, NULL, 0))
     {
@@ -26,12 +30,14 @@ int main()
         std::cout << "1. Enter data" << std::endl;
         std::cout << "2. Erase data" << std::endl;
         std::cout << "3. Update data" << std::endl;
-        std::cout << "4. Check data" << std::endl;
-        std::cout << "5. Quit" << std::endl;
+        std::cout << "4. Check name" << std::endl;
+        std::cout << "5. Check email" << std::endl;
+        std::cout << "6. Quit" << std::endl;
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
-        switch (choice) {
+        switch (choice) 
+        {
         case 1:
             enterData(connection);
             break;
@@ -42,16 +48,23 @@ int main()
             updateData(connection);
             break;
         case 4:
-            checkData(connection,);
+            std::cout << "Enter name to check: " << std::endl;
+            std::cin >> name;
+            checkName(connection, name);
             break;
         case 5:
+            std::cout << "Enter email to check: " << std::endl;
+            std::cin >> email;
+            checkName(connection, email);
+            break;
+        case 6:
             std::cout << "Goodbye!" << std::endl;
             break;
         default:
             std::cout << "Invalid choice. Please try again." << std::endl;
             break;
         }
-    } while (choice != 5);
+    } while (choice != 6);
 
     mysql_close(connection);
 
@@ -128,10 +141,41 @@ void updateData(MYSQL* connection)
     }
 }
 
-std::string checkData(MYSQL* connection, std::string& name)
+std::string checkName(MYSQL* connection, std::string& name)
 {
 
     std::string query = "SELECT * FROM mytable WHERE name = '" + name + "'";
+    if (mysql_query(connection, query.c_str()) != 0)
+    {
+        std::cout << "Error selecting from database: " << mysql_error(connection) << std::endl;
+        return "";
+    }
+
+    MYSQL_RES* result = mysql_store_result(connection);
+    if (result == NULL)
+    {
+        std::cout << "Error storing result: " << mysql_error(connection) << std::endl;
+        return "";
+    }
+
+    MYSQL_ROW row = mysql_fetch_row(result);
+    if (row == NULL)
+    {
+        std::cout << "Data not found in database." << std::endl;
+        return "";
+    }
+
+    std::string data = row[1];
+
+    mysql_free_result(result);
+    return data;
+
+}
+
+std::string checkEmail(MYSQL* connection, std::string& email)
+{
+
+    std::string query = "SELECT * FROM mytable WHERE name = '" + email + "'";
     if (mysql_query(connection, query.c_str()) != 0)
     {
         std::cout << "Error selecting from database: " << mysql_error(connection) << std::endl;
