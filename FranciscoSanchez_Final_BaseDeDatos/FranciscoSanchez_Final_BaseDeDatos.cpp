9148128a6b91729d8e75d917a61b7414d0345c13
pancho404 <franciscosanchez2101@gmail.com>
2023-02-24 16:17:53 -0300

    Made main function.

--- FranciscoSanchez_Final_BaseDeDatos/FranciscoSanchez_Final_BaseDeDatos.cpp
+++ FranciscoSanchez_Final_BaseDeDatos/FranciscoSanchez_Final_BaseDeDatos.cpp
@@ -11,7 +11,52 @@ void updateData(MYSQL* connection);
 void checkData(MYSQL* connection);
 
 
+int main()
+{
+    MYSQL* conn = mysql_init(NULL);
+    if (!mysql_real_connect(conn, "localhost", "root", "password", "mydb", 3306, NULL, 0))
+    {
+        std::cout << "Error connecting to database: " << mysql_error(conn) << std::endl;
+        return 1;
+    }
 
+    int choice = 0;
+    do {
+        std::cout << "MENU:" << std::endl;
+        std::cout << "1. Enter data" << std::endl;
+        std::cout << "2. Erase data" << std::endl;
+        std::cout << "3. Update data" << std::endl;
+        std::cout << "4. Check data" << std::endl;
+        std::cout << "5. Quit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            enterData(conn);
+            break;
+        case 2:
+            eraseData(conn);
+            break;
+        case 3:
+            updateData(conn);
+            break;
+        case 4:
+            checkData(conn);
+            break;
+        case 5:
+            std::cout << "Goodbye!" << std::endl;
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again." << std::endl;
+            break;
+        }
+    } while (choice != 5);
+
+    mysql_close(conn);
+
+    return 0;
+}
 
 
 void enterData(MYSQL* connection)
@@ -29,7 +74,7 @@ void updateData(MYSQL* connection)
 
 }
 
-void vheckData(MYSQL* connection)
+void checkData(MYSQL* connection)
 {
 
 }
